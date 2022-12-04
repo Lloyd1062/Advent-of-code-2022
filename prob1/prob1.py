83bcf1dcc6941482e6f702b7340ce07bf4bca8c0
Lloyd1062 <lloyd.williams1062@gmail.com>
2022-12-05 01:50:15 +1100

    😊

--- prob1/prob1.py
+++ prob1/prob1.py
@@ -1,10 +1,9 @@
 with open("input.txt", "r") as f:
     split = f.read().split("\n\n")
-    good = []
-    for i in range(len(split)):
-        good.append(sum(map(int,split[i].split("\n"))))
+    good = [sum(int(y) for y in x.split("\n")) for x in split]
 
 
 good.sort(reverse=True)
 print(sum([good[0], good[1], good[2]]))
 
+#PART 2
